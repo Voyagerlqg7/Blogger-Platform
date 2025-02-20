8cfa7242cbb2314278ca282f89c7bea94f084049
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-02-20 20:10:23 +0100

    Added testing DELETE ALL data

--- BloggerPlatform/src/index.ts
+++ BloggerPlatform/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import {BlogsRouter} from "./routes/BlogsRoutes";
 import {PostRouter} from "./routes/PostsRoutes";
+import {Request, Response} from "express";
+import {blogs} from "../src/DataB/Blogs"
+import {posts} from "../src/DataB/Posts"
+
 const app = express();
 const port = process.env.PORT || 6419;
 
@@ -8,6 +12,13 @@ app.use(express.json());
 
 app.use("/blogs", BlogsRouter);
 app.use("/posts", PostRouter)
+
+app.get('/testing/all-data',  (request: Request, response: Response) => {
+    blogs.length = 0;
+    posts.length = 0;
+});
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
