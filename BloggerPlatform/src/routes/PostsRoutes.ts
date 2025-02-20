e176a48cb700f20dde0373b8308e371ebc5efc9a
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-02-20 20:36:14 +0100

    Refactoring and FIX 0.1

--- BloggerPlatform/src/routes/PostsRoutes.ts
+++ BloggerPlatform/src/routes/PostsRoutes.ts
@@ -2,17 +2,18 @@ import {Request, Response, Router} from 'express';
 import {PostController} from "../Controllers/PostController";
 import {postValidationMiddleware} from "../Validator/PostsValidation";
 import {validationResult} from "express-validator";
-import {authMiddleware} from "../BasicAuthorization/authMiddleware"
+import {authMiddleware} from "../BasicAuthorization/authMiddleware";
 import {posts} from "../DataB/Posts";
 import {blogs} from "../DataB/Blogs";
 
 export const PostRouter = Router();
 
-
+// Public routes
 PostRouter.get('/', (request: Request, response: Response) => {
     const blogs = PostController.GetAllPosts();
     response.status(200).send(blogs);
 });
+
 PostRouter.get('/:id', (request: Request, response: Response) => {
     const blog = PostController.GetPostByID(request.params.id);
     if (blog) {
@@ -21,7 +22,9 @@ PostRouter.get('/:id', (request: Request, response: Response) => {
         response.status(404).send({ message: 'Post not found' });
     }
 });
-PostRouter.post('/', authMiddleware, postValidationMiddleware,(request: Request, response: Response) => {
+
+// Protected routes
+PostRouter.post('/', authMiddleware, postValidationMiddleware, (request: Request, response: Response) => {
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
         response.status(400).send({
@@ -30,13 +33,13 @@ PostRouter.post('/', authMiddleware, postValidationMiddleware,(request: Request,
                 field: err.type
             }))
         });
-    }
-    else{
+    } else {
         const newPost = PostController.AddNewPost(request.body);
         response.status(201).send(newPost);
     }
 });
-PostRouter.put('/:id',authMiddleware, (request: Request, response: Response) => {
+
+PostRouter.put('/:id', authMiddleware, (request: Request, response: Response) => {
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
         response.status(400).send({
@@ -53,15 +56,18 @@ PostRouter.put('/:id',authMiddleware, (request: Request, response: Response) => 
         response.status(404).send({ message: 'Post not found' });
     }
 });
+
 PostRouter.delete('/:id', authMiddleware, (request: Request, response: Response) => {
     const postToDelete = PostController.DeletePostByID(request.params.id);
-    if(postToDelete){
+    if (postToDelete) {
         response.status(204).send();
+    } else {
+        response.status(404).send({ message: 'Post not found' });
     }
-    else{response.status(404).send({ message: 'Post not found' });}
 });
-PostRouter.get('/testing/all-data', authMiddleware, (request: Request, response: Response) => {
-    posts.length = 0;
-    blogs.length = 0;
+
+PostRouter.delete('/testing/all-data', authMiddleware, (request: Request, response: Response) => {
+    blogs.length = 0; // Очищаем массив блогов
+    posts.length = 0; // Очищаем массив постов
     response.status(204).send();
 });
