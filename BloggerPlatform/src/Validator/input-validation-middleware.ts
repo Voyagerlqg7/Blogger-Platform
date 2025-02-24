5939193fe51cee46c0c3efe20a90f88356323bac
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-02-24 17:57:56 +0100

    FIX for tests

--- BloggerPlatform/src/Validator/input-validation-middleware.ts
+++ BloggerPlatform/src/Validator/input-validation-middleware.ts
@@ -4,13 +4,12 @@ import {validationResult} from "express-validator";
 export const inputValidationMiddleware = (request: Request, response: Response, next: NextFunction)=> {
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
-        const errorsMessages =  errors.array().map(error =>({
+        response.status(400).send({
+            errorsMessages : errors.array().map(error =>({
             message: error.msg,
             // @ts-ignore
             filed: error.path,
-        }));
-    response.status(400).send(errorsMessages.reverse());
+        }))}.errorsMessages.reverse());
     }
     else{next();}
-
 };
