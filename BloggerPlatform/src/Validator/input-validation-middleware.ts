64aec93900477fd540ce0595c6370dd4ee90696a
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-02-24 18:19:24 +0100

    FIX input-validation-middleware.ts

--- BloggerPlatform/src/Validator/input-validation-middleware.ts
+++ BloggerPlatform/src/Validator/input-validation-middleware.ts
@@ -1,15 +1,17 @@
-import {NextFunction, Request, Response} from "express";
-import {validationResult} from "express-validator";
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
 
-export const inputValidationMiddleware = (request: Request, response: Response, next: NextFunction)=> {
+export const inputValidationMiddleware = (request: Request, response: Response, next: NextFunction) => {
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
-        response.status(400).send({
-            errorsMessages : errors.array().map(error =>({
+
+        const errorsMessages = errors.array().map(error => ({
             message: error.msg,
             // @ts-ignore
-            filed: error.path,
-        }))}.errorsMessages.reverse());
+            field: error.path,
+        }));
+        response.status(400).json({ errorsMessages });
+    } else {
+        next();
     }
-    else{next();}
 };
