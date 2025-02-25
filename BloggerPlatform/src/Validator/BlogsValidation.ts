637651792f3cc0811dc5c8b9044cf3266ff63e99
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-02-25 17:13:09 +0100

    fix

--- BloggerPlatform/src/Validator/BlogsValidation.ts
+++ BloggerPlatform/src/Validator/BlogsValidation.ts
@@ -5,12 +5,14 @@ export const blogValidationMiddleware: ValidationChain[] = [
         .isString()
         .withMessage('Name must be a string')
         .trim()
+        .notEmpty()
         .isLength({ max: 15 })
         .withMessage('Name must be no longer than 15 characters'),
     body('description')
         .isString()
         .withMessage('Description must be a string')
         .trim()
+        .notEmpty()
         .isLength({ max: 500 })
         .withMessage('Description must be no longer than 500 characters'),
     body('websiteUrl')
@@ -18,6 +20,7 @@ export const blogValidationMiddleware: ValidationChain[] = [
         .withMessage('WebsiteUrl must be a string')
         .isURL()
         .trim()
+        .notEmpty()
         .withMessage('WebsiteUrl must be a valid URL')
         .isLength({ max: 100 })
         .withMessage('WebsiteUrl must be no longer than 100 characters'),
--- BloggerPlatform/src/Validator/PostsValidation.ts
+++ BloggerPlatform/src/Validator/PostsValidation.ts
@@ -6,17 +6,21 @@ export const postValidationMiddleware: ValidationChain[] = [
         .withMessage('Title must be a string')
         .isLength({ max: 30 })
         .trim()
+        .notEmpty()
         .withMessage('Title must be no longer than 30 characters'),
     body('shortDescription')
         .isString()
         .trim()
+        .notEmpty()
         .withMessage('Short description must be a string'),
     body('content')
         .isString()
         .trim()
+        .notEmpty()
         .withMessage('Content must be a string'),
     body('blogId')
         .isString()
         .trim()
+        .notEmpty()
         .withMessage('BlogId must be a string'),
 ];
