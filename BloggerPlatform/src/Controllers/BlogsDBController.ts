99f6c9f196ac19ba3e05ff3bb8d3dab7a505f771
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-03-20 18:05:11 +0100

    FIX converting between Mongo and app id\_id

--- BloggerPlatform/src/Controllers/BlogsDBController.ts
+++ BloggerPlatform/src/Controllers/BlogsDBController.ts
@@ -1,14 +1,15 @@
-import {BlogsDB} from "../Objects/Blogs";
-import {client} from "../mongo/ConnectDB"
+import { ObjectId } from 'mongodb'; // Добавьте импорт ObjectId
+import { BlogsDB } from "../Objects/Blogs";
+import { client } from "../mongo/ConnectDB";
 
 const blogDBController = client.db("BloggerPlatform").collection<BlogsDB>("blogs");
 
 export const BlogsDBController = {
-    async GetAllBlogs (): Promise<BlogsDB[]> {
+    async GetAllBlogs(): Promise<BlogsDB[]> {
         const blogs = await blogDBController.find().toArray();
 
         return blogs.map(blog => ({
-            id: blog._id.toString(),
+            id: blog._id.toString(), // Преобразуем _id в id
             name: blog.name,
             description: blog.description,
             websiteUrl: blog.websiteUrl,
@@ -16,34 +17,61 @@ export const BlogsDBController = {
             isMembership: blog.isMembership
         }));
     },
+
     async GetBlogByID(id: string): Promise<BlogsDB | undefined> {
         if (!id) return undefined;
-        return await blogDBController.findOne({id:id}) || undefined;
+
+        const blog = await blogDBController.findOne({ _id: new ObjectId(id) });
+
+        return blog ? {
+            id: blog._id.toString(), // Преобразуем _id в id
+            name: blog.name,
+            description: blog.description,
+            websiteUrl: blog.websiteUrl,
+            createdAt: blog.createdAt,
+            isMembership: blog.isMembership
+        } : undefined;
     },
-    async AddNewBlog (blog: BlogsDB): Promise<BlogsDB | undefined> {
-        let newBlog = {
-            id: new Date().getTime().toString(),
+
+    async AddNewBlog(blog: BlogsDB): Promise<BlogsDB | undefined> {
+        const newBlog = {
             name: blog.name,
             description: blog.description,
             websiteUrl: blog.websiteUrl,
             createdAt: new Date().toISOString(),
             isMembership: false
         };
+
         const result = await blogDBController.insertOne(newBlog);
-        return result.acknowledged ? newBlog : undefined;
+
+        return result.acknowledged ? {
+            id: result.insertedId.toString(),
+            ...newBlog
+        } : undefined;
     },
+
     async DeleteBlogByID(id: string | null): Promise<boolean> {
         if (!id) return false;
-        const result = await blogDBController.deleteOne({ id: id });
+
+        const result = await blogDBController.deleteOne({ _id: new ObjectId(id) });
         return result.deletedCount > 0;
     },
+
     async UpdateBlogByID(id: string, blog: BlogsDB): Promise<BlogsDB | undefined> {
         if (!id) return undefined;
+
         const updateResult = await blogDBController.findOneAndUpdate(
-            { id: id },
+            { _id: new ObjectId(id) },
             { $set: blog },
             { returnDocument: "after" }
         );
-        return updateResult || undefined;
+        return updateResult ? {
+            id: updateResult._id.toString(),
+            name: updateResult.name,
+            description: updateResult.description,
+            websiteUrl: updateResult.websiteUrl,
+            createdAt: updateResult.createdAt,
+            isMembership: updateResult.isMembership
+        } : undefined;
     }
 };
