77cfd4959f5adc4f33a72974b206d4451a3b4fc3
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-03-20 17:05:38 +0100

    FIX

--- BloggerPlatform/src/index.ts
+++ BloggerPlatform/src/index.ts
@@ -4,9 +4,6 @@ import {PostRouter} from "./routes/PostsRoutes";
 import {connectDB}from "./mongo/ConnectDB";
 import {client} from "./mongo/ConnectDB";
 
-
-
-
 const app = express();
 const port = process.env.PORT || 6419;
 app.use(express.json());
@@ -18,8 +15,8 @@ const startApp = async () => {
     await connectDB();
 
     app.delete('/testing/all-data', async (request: Request, response: Response) => {
-        await client.db("Blogs").collection("blogs").deleteMany({});
-        await client.db("Posts").collection("posts").deleteMany({});
+        await client.db("BloggerPlatform").collection("blogs").deleteMany({});
+        await client.db("BloggerPlatform").collection("posts").deleteMany({});
         response.status(204).send();
     });
 
