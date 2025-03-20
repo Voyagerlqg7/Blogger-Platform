1c826e09de7bc73f7dde89e74c7494084ea13b96
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-03-20 16:14:49 +0100

    added log

--- BloggerPlatform/src/mongo/ConnectDB.ts
+++ BloggerPlatform/src/mongo/ConnectDB.ts
@@ -13,13 +13,11 @@ export const client = new MongoClient(mongoURI);
 
 export async function connectDB() {
     try {
-         await client.connect();
-         console.log("Connected to DB successfully.");
-    }
-    catch (error) {
-        console.error("Error connecting to DB");
-        console.error(error);
-        await client.close();
+        await client.connect();
+        console.log("✅ Connected to MongoDB");
+    } catch (error) {
+        console.error("❌ Error connecting to MongoDB:", error);
+        process.exit(1); // Завершаем процесс
     }
 }
 
