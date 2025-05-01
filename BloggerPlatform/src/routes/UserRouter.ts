19bd6e5799b8079c65138672a57f8152a0c537aa
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-05-01 16:46:09 +0200

    little fix

--- BloggerPlatform/src/routes/UserRouter.ts
+++ BloggerPlatform/src/routes/UserRouter.ts
@@ -1,4 +1,4 @@
-import express = require("express");
+import express from "express";
 import {Request, Response, Router} from "express";
 import {usersValidationMiddleware} from "../Validator/UserValidation";
 import {authMiddleware} from "../BasicAuthorization/authMiddleware";
