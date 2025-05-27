f6a43e48747a034b3653088fdd1956868bb3b7a4
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-05-27 17:20:20 +0200

    FIX for CommentsRouter.ts

--- BloggerPlatform/src/routes/CommentsRouter.ts
+++ BloggerPlatform/src/routes/CommentsRouter.ts
@@ -2,6 +2,7 @@ import express, {Request, Response, Router} from "express";
 import {commentsValidationMiddleware} from "../Validator/CommentsValidation";
 import {CommentsService} from "../Domain/CommentsService";
 import {AuthMiddleware} from "../Authorization/AuthMiddleware";
+import {inputValidationMiddleware} from "../Validator/input-validation-middleware";
 
 
 export const CommentRouter = Router();
@@ -17,7 +18,7 @@ CommentRouter.get('/:id', async (req: Request, res: Response)=> {
         res.status(200).send(comment);
     }
 });
-CommentRouter.put('/:commentId', AuthMiddleware, commentsValidationMiddleware, async (req: Request, res: Response) => {
+CommentRouter.put('/:commentId', AuthMiddleware, commentsValidationMiddleware,inputValidationMiddleware, async (req: Request, res: Response) => {
     const commentId = req.params.commentId;
     const text = req.body.content;
     const userId = req.user!._id;
@@ -28,7 +29,7 @@ CommentRouter.put('/:commentId', AuthMiddleware, commentsValidationMiddleware, a
         res.status(403).send("Forbidden or comment not found"); // 403 — доступ запрещён
     }
     else{
-        res.status(200).send(updatedComment);
+        res.status(204).send(updatedComment);
     }
 });
 
@@ -37,11 +38,7 @@ CommentRouter.delete('/:commentId', AuthMiddleware, async (req: Request, res: Re
     const commentId = req.params.commentId;
     const result = await CommentsService.DeleteCommentById(commentId, userId);
     if (result === null) {
-        res.status(404).send("Comment not found");
-        return;
-    }
-    if (result === false) {
-        res.status(403).send("Forbidden: not your comment");
+        res.status(404).send("Forbidden or comment not found");
         return;
     }
     res.status(204).send();
