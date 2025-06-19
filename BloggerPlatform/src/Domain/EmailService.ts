a89f34d64d4dcbe96e4e0e52486ca021d146f13b
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-06-19 12:31:50 +0200

    Little update for EmailService.ts

--- BloggerPlatform/src/Domain/EmailService.ts
+++ BloggerPlatform/src/Domain/EmailService.ts
@@ -41,7 +41,9 @@ export const EmailService = {
         const newCode = uuidv4();
         const newExpiresAt = add(new Date(), { minutes: 5 }).toISOString();
 
-        if (!user) return false;
+        if (!user || user.emailConfirmation.isConfirmed) {
+            return false;
+        }
 
         await UsersDBController.UpdateCodeConfirmationAndExpiresTime(user._id, newCode, newExpiresAt);
         await this.SendEmailCodeConfirmation(user.accountData.login, user.accountData.email, newCode);
