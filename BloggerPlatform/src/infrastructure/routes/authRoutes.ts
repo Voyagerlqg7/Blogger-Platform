db5ea00d2a3cd7c8db6b0d442753e847fb91d172
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-08-14 16:29:32 +0200

    Onion architecture: Pre-final/ preparing some folders

--- BloggerPlatform/src/infrastructure/routes/authRoutes.ts
+++ BloggerPlatform/src/infrastructure/routes/authRoutes.ts
@@ -3,3 +3,11 @@ import {Router} from "express"
 
 const authRouter = Router();
 
+authRouter.post("/login", );
+authRouter.post("/registration",);
+authRouter.post("/registration-confirmation'", );
+authRouter.post('/registration-email-resending',);
+authRouter.post("/logout",);
+authRouter.post("/refresh-token",)
+authRouter.post("/me",)
+
