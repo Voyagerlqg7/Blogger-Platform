66d3e0a18f63007786d38bccd4a0d99f53515c93
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-09-10 17:01:41 +0200

    little fix in router

--- BloggerPlatform/src/infrastructure/controllers/AuthHTTPController.ts
+++ BloggerPlatform/src/infrastructure/controllers/AuthHTTPController.ts
@@ -147,7 +147,7 @@ export const aboutMeHandler = async (req: Request, res: Response) => {
         const user = req.user!;
 
         if (!user){
-            res.status(401)
+            res.sendStatus(401)
         }
         else {
             res.status(200).json({
