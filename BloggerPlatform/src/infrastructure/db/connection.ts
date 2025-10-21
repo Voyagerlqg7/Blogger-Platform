b1a5bc8b21a7b3dce200c5b3f874d71ab9f23b37
Vadim Stepanenko <stepanenko.vadik@bk.ru>
2025-10-21 17:40:21 +0200

    New connection.ts

--- BloggerPlatform/src/infrastructure/db/connection.ts
+++ BloggerPlatform/src/infrastructure/db/connection.ts
@@ -1,23 +1,27 @@
 import * as dotenv from 'dotenv';
-import {MongoClient} from "mongodb";
-import {settings} from "../settings/settings";
 import mongoose from "mongoose";
-
 dotenv.config();
-const mongoURI = settings.LOCAL_MONGODB_URI;
-export const client = new MongoClient(mongoURI);
 
-if(!mongoURI){
+import { settings } from "../settings/settings";
+
+const mongoURI =
+    settings.NODE_ENV === "development"
+        ? settings.LOCAL_MONGODB_URI
+        : settings.ATLAS_MONGO_URI;
+
+if (!mongoURI) {
     throw new Error("MongoDB URI is missing");
 }
+
+export const client = new mongoose.mongo.MongoClient(mongoURI);
 
 export async function connectDB() {
     try {
         await mongoose.connect(mongoURI);
-        client.connect()
-        console.log("Connected to MongoDB via Mongoose and raw client");
+        await client.connect();
+        console.log(`Connected to MongoDB via ${settings.NODE_ENV === "development" ? "local" : "Atlas"} database`);
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
-        process.exit(1); // Завершаем процесс
+        process.exit(1);
     }
 }
